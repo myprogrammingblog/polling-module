7055e013998b66194f594c6ee2feb6bdef1fc278
Justin Robinson <jtrobinson629@gmail.com>
2012-05-04 17:32:12 -0400

    Fixed config.sh typo

--- scripts/subscripts/config.sh
+++ scripts/subscripts/config.sh
@@ -10,14 +10,15 @@ fi
 echo " "
 
 if grep -q "<SERVER_IP>" ~/dev/bigbluebutton/bigbluebutton-client/src/conf/config.xml; then
-	echo "PollingModule is already looking at the right IP address or URL. Continuing with installation."
-else
 	serverURL=`grep "help url" ~/dev/bigbluebutton/bigbluebutton-client/src/conf/config.xml`
 	serverURL=${serverURL:22}
 	serverURL=${serverURL%"/help.html\"/>"}
-	sed -i s/\<SERVER_IP\>/$serverURL/g ~/dev/bigbluebutton/bigbluebutton-client/src/conf/config.xml
+	sed -i s/\<SERVER_IP\>/$serverURL/g ~/dev/bigbluebutton/bigbluebutton-client/src/conf/config.xml	
+else
+	echo "PollingModule is already looking at the right IP address or URL. Continuing with installation."
 fi
 
 echo " "
 echo "Modifications to config.xml complete."
 echo " "
+
