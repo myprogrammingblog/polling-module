d187dd6a2de0a2bc453b9a3dcb32df76d8535d28
Justin Robinson <jtrobinson629@gmail.com>
2012-05-08 14:30:22 -0400

    Added web polling to install script

--- scripts/install-polling-module.sh
+++ scripts/install-polling-module.sh
@@ -6,25 +6,26 @@ sudo chmod -R 755 ./subscripts
 # Make changes to config.xml
 cd "$present"
 cd subscripts
-sudo chmod -R 777 /var/www/bigbluebutton/client/conf/config.xml
 ./config.sh /var/www/bigbluebutton/client/conf/config.xml
 
 # Make changes to bbb-apps.xml
 cd "$present"
 cd subscripts
-sudo chmod -R 777 /usr/share/red5/webapps/bigbluebutton/WEB-INF/bbb-apps.xml
 ./bbb-apps.sh /usr/share/red5/webapps/bigbluebutton/WEB-INF/bbb-apps.xml
 
 # Move server-side class files
 cd "$present"
 cd ../prod_files
 sudo /etc/init.d/red5 stop
-sudo chmod -R 777 /usr/share/red5/webapps/bigbluebutton/WEB-INF/classes
-cp -r classes /usr/share/red5/webapps/bigbluebutton/WEB-INF/
+sudo cp -r classes /usr/share/red5/webapps/bigbluebutton/WEB-INF/
 sudo bbb-conf --clean
 
 # Move client-side SWF files
 cd "$present"
 cd ../prod_files
-sudo chmod -R 777 /var/www/bigbluebutton/client
-cp -r client /var/www/bigbluebutton/
+sudo cp -r client /var/www/bigbluebutton/
+
+# Install web polling
+cd "$present"
+cd subscripts
+./web-polling.sh
--- scripts/subscripts/config.sh
+++ scripts/subscripts/config.sh
@@ -3,8 +3,8 @@
 if grep -q "PollingModule" $1; then
 	echo "PollingModule already exists in config.xml. Continuing with installation."
 else
-	sed -i 's/<\/modules>/\t<module name=\"PollingModule\" \n\t\t\turl=\"PollingModule.swf?v=3809\" \n\t\t\turi="rtmp:\/\/<SERVER_IP>\/bigbluebutton" \n\t\t\thost="http:\/\/<SERVER_IP>" \n\t\t\tdependsOn="ViewersModule" \n\t\t\/> \n\t<CLOSE-MODULE>/' $1
-	sed -i 's/<CLOSE-MODULE>/<\/modules>/' $1
+	sudo sed -i 's/<\/modules>/\t<module name=\"PollingModule\" \n\t\t\turl=\"PollingModule.swf?v=3809\" \n\t\t\turi="rtmp:\/\/<SERVER_IP>\/bigbluebutton" \n\t\t\thost="http:\/\/<SERVER_IP>" \n\t\t\tdependsOn="ViewersModule" \n\t\t\/> \n\t<CLOSE-MODULE>/' $1
+	sudo sed -i 's/<CLOSE-MODULE>/<\/modules>/' $1
 fi
 
 echo " "
@@ -14,7 +14,7 @@ if grep -q "<SERVER_IP>" $1; then
 	serverURL=${serverURL:22}
 	serverURL=${serverURL%"/help.html\"/>"}
 	echo "Server url is $serverURL"
-	sed -i s/\<SERVER_IP\>/$serverURL/g $1	
+	sudo sed -i s/\<SERVER_IP\>/$serverURL/g $1	
 else
 	echo "PollingModule is already looking at the right IP address or URL. Continuing with installation."
 fi
