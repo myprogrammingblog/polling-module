0864db12d9fbced54fbdc8f2d092860b71a1023b
Justin Robinson <jtrobinson629@gmail.com>
2012-05-08 11:22:17 -0400

    Updated some scripts to have a -dev suffix for development setup

---
+++ scripts/install-polling-module-dev.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+clear
+present=`pwd`
+sudo chmod -R 755 ./subscripts
+
+# Make changes to build.xml
+cd subscripts
+./build.sh
+
+# Make changes to config.xml
+cd "$present"
+cd subscripts
+./config-dev.sh
+
+# Make changes to Images.as
+cd "$present"
+cd subscripts
+./image-dep.sh
+
+# Make changes to locales
+cd "$present"
+cd subscripts
+./locales.sh
+
+# Make changes to bbb-apps.xml
+cd "$present"
+cd subscripts
+./bbb-apps-dev.sh
+
+# Install web polling
+cd "$present"
+cd subscripts
+./web-polling.sh
+
+# Graft polling module file structure onto main BBB file structure
+cd "$present"
+cd ..
+sudo chmod -R 777 ~/dev/bigbluebutton
+cp -r bigbluebutton ~/dev/
+read -p "Press any key to continue... " -n1 -s
+echo " "
+
+# Build locales
+repeat=true
+while $repeat; do
+    cd ~/dev/bigbluebutton/bigbluebutton-client
+    ant locales
+    echo "Was the build successful?"
+    select response in "Yes" "No"; do
+        case $response in
+	    Yes ) repeat=false; break;;
+            No ) break;;
+        esac
+    done
+done
+echo " "
+
+# Build client
+repeat=true
+while $repeat; do
+    cd ~/dev/bigbluebutton/bigbluebutton-client
+    ant
+    echo "Was the build successful?"
+    select response in "Yes" "No"; do
+        case $response in
+	    Yes ) repeat=false; break;;
+            No ) break;;
+        esac
+    done
+done
+echo " "
+
+# Stop Red5 and compile apps
+sudo /etc/init.d/red5 stop
+cd /home/firstuser/dev/bigbluebutton/bigbluebutton-apps
+gradle resolveDeps
+gradle clean war deploy
+read -p "Press any key to continue... " -n1 -s
+echo " "
+
+# Clean restart of Big Blue Button
+sudo service nginx restart
+sudo bbb-conf --clean
---
+++ scripts/subscripts/bbb-apps-dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if grep -q 'bean id="pollHandler"' ~/dev/bigbluebutton/bigbluebutton-apps/src/main/webapp/WEB-INF/bbb-apps.xml; then
+	echo "Polling Module beans already exist in bbb-apps.xml."
+else
+	sed -i 's/<\/beans>/\t<!-- START Polling  -->\n\t<bean id="pollHandler"\n\t      class="org.bigbluebutton.conference.service.poll.PollHandler">\n\t\t<property name="pollApplication"> \n\t\t\t<ref local="pollApplication"\/>\n\t\t<\/property>\n\t\t<property name="recorderApplication"> \n\t\t\t<ref local="recorderApplication"\/>\n\t\t<\/property>\n\t<\/bean>\n\t\n\t<bean id="pollApplication" class="org.bigbluebutton.conference.service.poll.PollApplication">\n\t\t<property name="roomsManager"> \n\t\t\t<ref local="pollRoomsManager"\/>\n\t\t<\/property>\n\t<\/bean>\n\t\n\t<bean id="poll.service" \n\t      class="org.bigbluebutton.conference.service.poll.PollService">\n\t\t<property name="pollApplication"> \n\t\t\t<ref local="pollApplication"\/>\n\t\t<\/property>\n\t<\/bean>\n\t\n\t<bean id="pollRoomsManager"\n\t      class="org.bigbluebutton.conference.service.poll.PollRoomsManager"\/>\n\t\n\t<bean id="pollRecorder"\n\t      class="org.bigbluebutton.conference.service.recorder.polling.PollRecorder">\n\t\t<property name="redisPool">\n\t\t\t<ref local="redisPool" \/> \n\t\t<\/property>   \n\t<\/bean>\n<\/beans>/' ~/dev/bigbluebutton/bigbluebutton-apps/src/main/webapp/WEB-INF/bbb-apps.xml
+	#sed -i 's/<\/beans>/HELLO/' ~/dev/bigbluebutton/bigbluebutton-apps/src/main/webapp/WEB-INF/bbb-apps.xml
+fi
+
+echo " "
+echo "Modifications to bbb-apps.xml complete."
+echo " "
